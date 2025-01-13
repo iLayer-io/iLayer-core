e9ab58514aef815f41e9968c7917653f6de769fe
Synaptic0n <riccardo.tosti92@gmail.com>
2025-01-13 19:25:19 +0100

    fmt: format mock

--- test/mocks/MockERC1155.sol
+++ test/mocks/MockERC1155.sol
@@ -6,12 +6,7 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 contract MockERC1155 is ERC1155 {
     constructor(string memory uri) ERC1155(uri) {}
 
-    function mint(
-        address to,
-        uint256 id,
-        uint256 amount,
-        bytes memory data
-    ) external {
+    function mint(address to, uint256 id, uint256 amount, bytes memory data) external {
         _mint(to, id, amount, data);
     }
 }
